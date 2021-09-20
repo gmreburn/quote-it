c05a8c6e6716eaaaf317067489e5f928bb4c07a6
Gregory Reburn <gmreburn@gmail.com>
2021-09-20 17:43:28 -0500

    use sidebar related theme colors (#10)
    * use sidebar related theme colors
    
    * allow up to 5 lines of a quote to shown
    
    * use tailwind JIT compiling
    
    * render app to div instead of body
    
    * respond to theme update event
    
    * bump version
    
    * bump browser version requirement for theme onupdate support

--- src/sidebar/components/NoQuotesYet.jsx
+++ src/sidebar/components/NoQuotesYet.jsx
@@ -3,13 +3,13 @@ import { FolderAddIcon } from "@heroicons/react/outline";
 
 function NoQuotesYet() {
 	return (
-		<div className="flex justify-center mt-20">
+		<div className="flex justify-center mt-4">
 			<div>
-				<FolderAddIcon className="mx-auto h-12 w-12 text-gray-400" />
-				<h3 className="mt-2 font-bold text-gray-900 text-center">
+				<FolderAddIcon className="mx-auto h-12 w-12" />
+				<h3 className="mt-2 font-bold text-center">
 					{browser.i18n.getMessage("NoQuotesYet")}
 				</h3>
-				<p className="mt-1 mx-8 text-gray-500 text-center">
+				<p className="mt-1 mx-8 text-center">
 					{browser.i18n.getMessage("NoQuotes_GetStarted")}
 				</p>
 			</div>
--- src/sidebar/panel.jsx
+++ src/sidebar/panel.jsx
@@ -3,9 +3,31 @@ import ReactDOM from "react-dom";
 import "../tailwind.css";
 import Sidebar from "./components/Sidebar.jsx";
 
+function setSidebarStyle(theme) {
+	if (theme.colors) {
+		document.body.style = `background: ${theme.colors.frame}; color: ${theme.colors.sidebar_text}; border-color: ${theme.colors.sidebar_border}`;
+	} else {
+		document.body.style = ``;
+	}
+}
+
 browser.windows.getCurrent({ populate: true }).then((windowInfo) => {
+	var appElement = document.createElement("div");
+	document.body.appendChild(appElement);
 	ReactDOM.render(
 		<Sidebar tab={windowInfo.tabs.find((tab) => tab.active)} />,
-		document.body
+		appElement
 	);
+	browser.theme.onUpdated.addListener(({ theme, windowId }) => {
+		/*
+		  Only update theme if it applies to the window the sidebar is in.
+		  If a windowId is passed during an update, it means that the theme is applied to that specific window.
+		  Otherwise, the theme is applied globally to all windows.
+		*/
+		if (!windowId || windowId == windowInfo.id) {
+			setSidebarStyle(theme);
+		}
+	});
 });
+
+browser.theme.getCurrent().then(setSidebarStyle);

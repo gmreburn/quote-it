4a388a3392ad694a870f9d2449d16a3581a1d7ac
Gregory Reburn <gmreburn@gmail.com>
2021-09-20 12:52:26 -0500

    use sidebar related theme colors allow up to 5 lines of a quote to shown use tailwind JIT compiling render app to div instead of body

--- src/sidebar/panel.jsx
+++ src/sidebar/panel.jsx
@@ -4,8 +4,17 @@ import "../tailwind.css";
 import Sidebar from "./components/Sidebar.jsx";
 
 browser.windows.getCurrent({ populate: true }).then((windowInfo) => {
+	browser.theme.getCurrent().then((styles) => {
+		console.log(styles.colors);
+		if (styles.colors) {
+			document.body.style = `background: ${styles.colors.sidebar}; color: ${styles.colors.sidebar_text}; border-color: ${styles.colors.sidebar_border}`;
+			// TODO: respect color from styles.colors.icons for icons
+		}
+	});
+	var appElement = document.createElement("div");
+	document.body.appendChild(appElement);
 	ReactDOM.render(
 		<Sidebar tab={windowInfo.tabs.find((tab) => tab.active)} />,
-		document.body
+		appElement
 	);
 });

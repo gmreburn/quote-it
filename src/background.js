bb83dc169b2fc95e785ecf5a4132d8ce3d58fad1
Gregory Reburn <gmreburn@gmail.com>
2021-09-24 19:25:13 -0500

    notify on create

--- src/background.js
+++ src/background.js
@@ -1,4 +1,3 @@
-import { nanoid } from "nanoid";
 import QuoteAPI from "./api/QuoteAPI";
 
 browser.menus.create({
@@ -12,18 +11,20 @@ browser.menus.onClicked.addListener(function (info, tab) {
 		console.log("save selection", info, tab);
 		QuoteAPI()
 			.create(info.selectionText, tab)
-			.then((quote) =>
-				browser.runtime.sendMessage({ type: "QUOTE_ADDED", quote })
-			)
-			.then(() => {
-				browser.notifications.create(`${quote.id}-created`, {
-					type: "basic",
-					title: browser.i18n.getMessage("QuoteAddedTitle"),
-					message: browser.i18n.getMessage("QuoteAddedMessage"),
-				});
-				setTimeout(() => {
-					browser.notifications.clear(`${quote.id}-created`);
-				}, 7000);
+			.then((quote) => {
+				browser.runtime.sendMessage({ type: "QUOTE_ADDED", quote });
+
+				browser.notifications
+					.create(`${quote.id}-created`, {
+						type: "basic",
+						title: browser.i18n.getMessage("QuoteAddedTitle"),
+						message: browser.i18n.getMessage("QuoteAddedMessage"),
+					})
+					.then(() =>
+						setTimeout(() => {
+							browser.notifications.clear(`${quote.id}-created`);
+						}, 7000)
+					);
 			});
 	}
 });

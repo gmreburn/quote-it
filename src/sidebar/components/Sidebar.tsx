715538f204841a7efae6cde4b6cdf1271620c573
Gregory Reburn <greg@reburn.io>
2024-01-14 18:26:46 -0600

    checkpoint

--- src/sidebar/components/Sidebar.tsx
+++ src/sidebar/components/Sidebar.tsx
@@ -4,10 +4,18 @@ import useQuotes from "../../hooks/useQuotes";
 import NoQuotesYet from "./NoQuotesYet";
 import QuoteProvider from "providers/QuoteProvider";
 import useTab from "hooks/useTab";
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from "components/ui/collapsible";
+import { Button } from "components/ui/button";
+import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 
 function Sidebar() {
 	const [tab] = useTab();
 	const [quotes] = useQuotes(tab.url);
+	const [isOpen, setIsOpen] = React.useState(false);
 
 	if (!Array.isArray(quotes)) {
 		return null;
@@ -15,13 +23,41 @@ function Sidebar() {
 		return <NoQuotesYet />;
 	} else {
 		return (
-			<ul role='list' className='divide-y divide-gray-200 dark:divide-gray-800'>
-				{quotes.map((quote) => (
-					<QuoteProvider quote={quote} key={quote.id}>
-						<SidebarQuote />
-					</QuoteProvider>
-				))}
-			</ul>
+			<>
+				<Collapsible
+					open={isOpen}
+					onOpenChange={setIsOpen}
+					className='space-y-2'
+				>
+					<div className='flex items-center justify-between space-x-4 px-4 hover:bg-zinc-200'>
+						<CollapsibleTrigger asChild>
+							<Button variant='ghost' size='sm' className='w-9 p-0'>
+								{/* <ChevronUpDownIcon /> */}
+								{isOpen ? (
+									<ChevronDownIcon className='h-4 w-4' />
+								) : (
+									<ChevronRightIcon className='h-4 w-4' />
+								)}
+								<span className='sr-only'>Toggle</span>
+							</Button>
+						</CollapsibleTrigger>
+						<h4 className=''>Grouped by something</h4>
+					</div>
+					<CollapsibleContent className='space-y-2'>
+						{/* <div className='rounded-md border px-4 py-3 font-mono text-sm'>
+							Mock Quote 1
+						</div>
+						<div className='rounded-md border px-4 py-3 font-mono text-sm'>
+							Mock Quote 2
+						</div> */}
+						{quotes.map((quote) => (
+							<QuoteProvider quote={quote} key={quote.id}>
+								<SidebarQuote />
+							</QuoteProvider>
+						))}
+					</CollapsibleContent>
+				</Collapsible>
+			</>
 		);
 	}
 }
